ded3751da058368fa0decaf89b5746b4fc0cf659
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2023-11-09 16:54:44 +0530

    Add

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.json());
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 app.use(express.json());
@@ -25,6 +27,35 @@ app.get("/task2/about", (req, res) => {
 app.get("/task2/blog", (req, res) => {
   res.render("blog");
 });
+app.get("/task3", (req, res) => {
+  res.render("bmi");
+});
+
+app.post("/task3", (req, res) => {
+  let height = parseFloat(req.body.height);
+  let unit = req.body.unit;
+  let weight = parseFloat(req.body.weight);
+
+  if (unit === 'in') {
+    height *= 0.0254;
+  } else if (unit === 'ft') {
+    height *= 0.3048;
+  }
+
+
+
+  const bmi = weight / (height * height);
+
+  res.render("bmi", {
+    bmi: bmi.toFixed(2)
+  });
+});
+
+
+
+
+
+
 
 
 app.listen(3000, () => {
