4850665c3812fe3c9b1501d90d85ee0274192cfb
Sreejith2 <99952612+Sreejith2@users.noreply.github.com>
2023-11-09 19:30:43 +0530

    Add

--- index.js
+++ index.js
@@ -36,13 +36,11 @@ app.post("/task3", (req, res) => {
   let unit = req.body.unit;
   let weight = parseFloat(req.body.weight);
 
-  if (unit === 'in') {
-    height *= 0.0254;
+  if (unit === 'cm') {
+    height *= 0.01;
   } else if (unit === 'ft') {
     height *= 0.3048;
   }
-
-
 
   const bmi = weight / (height * height);
 
@@ -51,7 +49,27 @@ app.post("/task3", (req, res) => {
   });
 });
 
+app.get("/task4", (req, res) => {
+  res.render("task4");
+});
 
+app.post("/task4",(req,res)=>{
+  const fname="",lname="";
+  let name = req.body.name;
+  let dob = req.body.dob;
+  let address = req.body.address;
+  let pin = req.body.pincode;
+  let phone = req.body.phone;
+  const names = name.split(" ");
+  if(names.length>1){
+    fname = names.slice(0, -1).join(' ');
+    lname = names[names.length-1];
+  }else{
+    fname=name;
+    lname="";
+  }
+  console.log(name,dob,address,pin,phone);
+});
 
 
 
